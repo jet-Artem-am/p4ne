577abc9d292952f1a595125bfda1ee7d49dbf449
aa.morzherin <distr40@gmail.com>
2021-03-16 18:09:08 +0300

    Lab 1.2_upd

--- Lab1.2/SecondLab.py
+++ Lab1.2/SecondLab.py
@@ -1,20 +1,21 @@
-# Вторая программа
+#Вторая программа
 
 from matplotlib import pyplot
 from openpyxl import load_workbook
 
-def getvalue(a):
-    return a.value
+def getvalue(x):
+    return x.value
 
 wb = load_workbook('data_analysis_lab.xlsx')
+
 lict = wb['Data']
 
 years = list(map(getvalue, lict['A'][1:]))
 relative_temp = list(map(getvalue, lict['C'][1:]))
 activity = list(map(getvalue, lict['D'][1:]))
 
-pyplot.plot(years, relative_temp, label='Относительная температура')
-pyplot.plot(years, activity, label= 'Солнечная активность')
+pyplot.plot(years, relative_temp, label="Относительная температура")
+pyplot.plot(years, activity, label="Солнечная активность")
 
 pyplot.xlabel('Год')
 pyplot.ylabel('Температура/Солнечная активность')
