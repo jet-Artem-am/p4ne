41bb0724052720669f62b24204df0c01a7a652ff
aa.morzherin <distr40@gmail.com>
2021-03-19 16:07:49 +0300

    Lab 1.5

---
+++ Lab1.5/FifthLab.py
@@ -0,0 +1,14 @@
+# Пятая программа
+
+import glob
+
+ip_set = set()
+
+for current_f_name in glob.glob("/Users/aa.morzherin/Documents/p4ne_training/config_files/*.txt"):
+    with open(current_f_name) as a:
+        for cur_line in a:
+            if cur_line.find("ip address") == 1:
+                ip_set.add(cur_line.replace("ip address", "").strip())
+
+for n in ip_set:
+    print(n)
